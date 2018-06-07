5efb61a611b18e6a9eeea114d0f44fdfa5f59e2b
Aaron Faucher <aaron.faucher@gmail.com>
2018-06-06 23:17:18 -0400

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Test : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    private void DoTriggerClicked(object sender, ControllerInteractionEventArgs e)
+    {
+        Debug.Log("TEST");
+    }
+
+    public void TestMethod()
+    {
+        Debug.Log("TEST");
+
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+
+
+}
