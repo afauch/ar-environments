e430a9adc3563264e50970b1b66aab691affea06
Aaron Faucher <aaron.faucher@gmail.com>
2018-06-13 01:04:04 -0400

    Added Leap Motion Dynamic Hands

---
+++ Assets/InteractionManagerTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InteractionEngineUtility;
+using Leap.Unity.Attributes;
+using Leap.Unity.RuntimeGizmos;
+using Leap.Unity.Query;
+using Leap.Unity.Interaction.Internal;
+using Leap.Unity.Interaction;
+
+public class InteractionManagerTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
