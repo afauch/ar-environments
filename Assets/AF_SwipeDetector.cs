fa55e83afd7070df00f05a42e51f42ced909d5fb
Aaron Faucher <aaron.faucher@gmail.com>
2018-06-28 22:00:41 -0400

    Functional Stencil Carousel

--- Assets/AF_SwipeDetector.cs
+++ Assets/AF_SwipeDetector.cs
@@ -9,6 +9,13 @@ public class AF_SwipeDetector : MonoBehaviour {
     public Transform _palmFacingTransform;
     public GameObject _tube;
 
+    public GameObject[] _tubeComponents;
+    private int _currentTubeComponentIndex = 0;
+    public float _tweenTime = 1.2f;
+
+    public AudioSource _audioSource;
+
+
     private void Update()
     {
         // GetCouldSwipe();
@@ -19,7 +26,29 @@ public class AF_SwipeDetector : MonoBehaviour {
         Debug.Log("All Extended.");
         _isExtended = true;
 
-        _tube.SetRelativeRotation(new Vector3(0.0f, -90.0f, 0.0f));
+        // Rotate the chassis
+        _tube.SetRelativeRotation(new Vector3(0.0f, 90.0f, 0.0f), _tweenTime, Easing.BounceOut);
+        _audioSource.Play();
+
+        Debug.Log(_tubeComponents[_currentTubeComponentIndex].name);
+
+        // Fade Out the current one
+        _tubeComponents[_currentTubeComponentIndex].SetOpacity(0.0f, _tweenTime);
+        _tubeComponents[_currentTubeComponentIndex].SetPosition(new Vector3(0.0f, 0.0f, 0.0f), _tweenTime, Easing.QuarticIn);
+
+
+        if (_currentTubeComponentIndex == 0)
+        {
+            _currentTubeComponentIndex = 3;
+        } else
+        {
+            _currentTubeComponentIndex -= 1;
+        }
+
+        // Fade in the new component
+        _tubeComponents[_currentTubeComponentIndex].SetOpacity(1.0f, _tweenTime);
+        _tubeComponents[_currentTubeComponentIndex].SetPosition(new Vector3(0.0f, 6.0f, 0.0f), _tweenTime, Easing.CubicOut);
+
 
     }
 
