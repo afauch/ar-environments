b383014eff8dec27bdd0d80eb72be796992acb98
Aaron Faucher <aaron.faucher@gmail.com>
2018-07-02 21:46:29 -0400

    Finally getting good angles

--- Assets/AF_SwipeDetector.cs
+++ Assets/AF_SwipeDetector.cs
@@ -8,12 +8,15 @@ public class AF_SwipeDetector : MonoBehaviour {
     private bool _isExtended = false;
     public Transform _palmFacingTransform;
     public GameObject _tube;
+    public Transform _bodyCenter;
 
     public GameObject[] _tubeComponents;
     private int _currentTubeComponentIndex = 0;
     public float _tweenTime = 3.0f;
 
-    public LineRenderer _lr;
+    public LineRenderer _lr1;
+    public LineRenderer _lr2;
+    public LineRenderer _lr3;
 
     public AudioSource _audioSource;
 
@@ -48,11 +51,7 @@ public class AF_SwipeDetector : MonoBehaviour {
 
         if(_isExtended)
         {
-            _positionDelta = Vector3.Distance(_initPosition, _palmFacingTransform.transform.position);
-            Debug.Log(_positionDelta);
-
-            _lr.SetPosition(0, _tube.transform.position);
-            _lr.SetPosition(1, _palmFacingTransform.transform.position);
+            GetSignedAngle(_bodyCenter.transform, _palmFacingTransform);
         }
 
     }
@@ -85,10 +84,31 @@ public class AF_SwipeDetector : MonoBehaviour {
 
     }
 
-    private void GetSignedAngle()
+    private void GetSignedAngle(Transform center, Transform satellite)
     {
+        //Vector3 centerPos = center.position;
+        //Vector3 satellitePos = new Vector3 (satellite.position.x, center.position.y, satellite.position.z);
+        //Vector3 cornerPos = center.forward * satellitePos.z;
 
-        
+        //DrawLine(_lr1, centerPos, satellitePos);
+        //DrawLine(_lr2, satellitePos, cornerPos);
+        //DrawLine(_lr3, centerPos, cornerPos);
+
+        Vector3 targetDir = new Vector3 (satellite.position.x, 0.0f, satellite.position.z) - new Vector3(center.position.x, 0.0f, center.position.z);
+        // normalize the world rotation
+
+        Vector3 forward = center.transform.forward;
+        forward.y = 0.0f;
+
+        float angleBetween = Vector3.SignedAngle(forward, targetDir, Vector3.up);
+        Debug.Log(angleBetween);
+
+    }
+
+    private void DrawLine(LineRenderer lr, Vector3 point1, Vector3 point2)
+    {
+        lr.SetPosition(0, point1);
+        lr.SetPosition(1, point2);
     }
 
 }
